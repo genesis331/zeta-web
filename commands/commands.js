9f2f9fbf12433f5baf5b98392213f74adfa06c5c
genesis331 <cheah3838@gmail.com>
2018-03-13 15:38:23 +0800

    Make editing more easier

--- commands/commands.js
+++ commands/commands.js
@@ -4,3 +4,13 @@ $(window).on("load", function() {
     $(".c3").addClass("show");
     $(".c4").addClass("show");
 });
+
+var http = new XMLHttpRequest();
+http.overrideMimeType("application.json");
+http.open('GET','https://genesis331.github.io/zeta-web/commands/commands.json',true);
+http.onreadystatechange = function() {
+    if (http.readyState === 4 && http.status === 200) {
+        var data = http.responseText;
+    }
+}
+http.send();
