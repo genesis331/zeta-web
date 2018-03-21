4dba2d893c7205d508c5d048489001b5b24cc054
genesis331 <cheah3838@gmail.com>
2018-03-21 21:26:14 +0800

    Added contents to Changelog Page

--- changelog/changelog.js
+++ changelog/changelog.js
@@ -1,1 +1,14 @@
-$(window).on("load", function() {})
+$(window).on("load", function() {
+    $(".content").addClass("show");
+})
+
+var http = new XMLHttpRequest();
+http.open('GET','https://genesis331.github.io/zeta-bot/version.json',true);
+http.onreadystatechange = function() {
+    if (http.readyState === 4 && http.status === 200) {
+        var data = JSON.parse(this.responseText);
+        $(".c1").text("Current Version: " + data.current_version);
+        $(".c2").text(data.current_changelog);
+    }
+}
+http.send();
