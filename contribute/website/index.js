2dff41551a5069a6c1b06d18fcc632cc09d40f28
genesis331 <cheah3838@gmail.com>
2018-04-06 20:36:08 +0800

    Added a nice footer for Contribute Pages

--- contribute/website/index.js
+++ contribute/website/index.js
@@ -1,3 +1,12 @@
 $(window).on("load", function() {
     $(".content").addClass("show");
+    $(".f1cc").mouseover(function() {
+        $(".f2b").addClass("show");
+    });
+    $(".f1cc").mouseout(function() {
+        $(".f2b").removeClass("show");
+    });
+    $(".f1cc").on("click", function() {
+        window.open('../','_self');
+    })
 });
